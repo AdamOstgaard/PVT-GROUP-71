6d9be246004526998f1cace98387ff2a71d10142
AdamOstgaard <adam.ostgaard@gmail.com>
2019-05-14 18:42:02 +0200

    feat: move toggle logic into button component. feat: formatted documents

--- pvt71/components/TimerSleepButton.js
+++ pvt71/components/TimerSleepButton.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import { TouchableOpacity, StyleSheet, Image}  from 'react-native';
+import React from "react";
+import { TouchableOpacity, StyleSheet, Image } from "react-native";
 
 export default class TimerSleepButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state({
+      enabled: false
+    });
+  }
+    
+  render() {
+    return (
+      <TouchableOpacity style={styles.button} onPress={}>
+        <Image source={require("../assets/images/zzz-icon-3.png")} />
+      </TouchableOpacity>
+    );
+  }
 
-    render(){
-        return(
-            <TouchableOpacity 
-                style={styles.button}
-                onPress={this.props.onPress}
-            >
-                <Image source={require('../assets/images/zzz-icon-3.png')}/>
-            </TouchableOpacity>
-        );
-    }
+  toggle() {
+      const val = !this.state.enabled;
+      this.props.onPress(val)
+      this.setState({ enabled: val });
+  }
 }
 
-const styles= StyleSheet.create({
-    button: {
-        height: 90,
-        width: 90,
-    },
-    
+const styles = StyleSheet.create({
+  button: {
+    height: 90,
+    width: 90
+  }
 });
