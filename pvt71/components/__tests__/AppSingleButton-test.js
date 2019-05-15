0c4609b4c715e60816b48dca8106dfe1d9af045d
Vonskareng <rebecka.skareng@gmail.com>
2019-05-15 13:52:13 +0200

    feat: added simple test and snapshot for AppSingleButton

---
+++ pvt71/components/__tests__/AppSingleButton-test.js
@@ -0,0 +1,11 @@
+import "react-native";
+import React from "react";
+import  AppSingleButton  from "../AppSingleButton";
+import renderer from "react-test-renderer";
+
+describe("AppSingleButton Test", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<AppSingleButton title='Next'/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
