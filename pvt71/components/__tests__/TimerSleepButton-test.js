a6fcf7417031d344ad1f66e8b63eda85cc33636e
Adam Östgaard <adam.ostgaard@gmail.com>
2019-05-21 14:54:13 +0200

    test: mock alert

--- pvt71/components/__tests__/TimerSleepButton-test.js
+++ pvt71/components/__tests__/TimerSleepButton-test.js
@@ -14,6 +14,13 @@ describe("Timer Test", () => {
   });
 
   describe("test toggle()", () => {
+        // Mock the alert to avoid having the real alert keeping the test suite open.
+        jest.mock("Alert", () => {
+          return { 
+            alert: jest.fn()
+          };
+        });
+    
     it("should update state variable enable to true", () => {
       let button = renderer
         .create(
@@ -28,13 +35,4 @@ describe("Timer Test", () => {
     });
   });
 
- // describe("test componentDidUpdate()", () => {
-  //  it("should not update variable enable to true", () => {
-  //    let button = renderer
-  //      .create(<TimerSleepButton onToggle={true} onResume={true} />)
-  //      .getInstance();
-  //    button.componentDidUpdate(button.props.onResume);
-  //    expect(button.state.enabled).toEqual(false);
-  //  });
- // });
 });
