d5314dc312012298b5526e65dfd14212eb9f9106
Hoyko <44168820+Hoyko@users.noreply.github.com>
2019-05-26 14:52:33 +0200

    feat/Added-basic-GUI-for-Wizard
    feat: Added the basic GUI for the wizard.

--- pvt71/screens/WizardSettingsScreen.js
+++ pvt71/screens/WizardSettingsScreen.js
@@ -1,39 +1,146 @@
 import React from "react";
 import AppSingleButton from "../components/AppSingleButton";
 import { StyleSheet, Text, View } from "react-native";
+import TimePicker from "react-native-simple-time-picker"
+import { AsyncStorage } from "react-native";
+import moment from "moment";
 
 export default class WizardVerifyContactScreen extends React.Component {
+  state = {
+    selectedHours: 5,
+    SelectedMinutes: 0,
+    warningHours: 0,
+    warningMinutes: 30,
+  };
+  
   render() {
+    const { selectedHours, selectedMinutes, warningHours, warningMinutes } = this.state;
     return (
       <View style={styles.container}>
-        <Text style={styles.heading}>Wizard Settings Screen.</Text>
-        <View style={styles.bottom}>
-          <AppSingleButton
-            title="Next"
-            onPress={() =>
-              this.props.navigation.navigate("HomeScreen")
+        <View style={styles.boxContainerTop}>
+          <Text style={styles.heading}>Timerinställningar</Text>
+          <Text style={styles.infoText}>
+            Här kan du ändra hur ofta du vill verifiera ditt välmående
+          </Text>
+        </View>
+        <View style={styles.boxContainerMid1}>
+          <Text style={styles.picker}>Timmar</Text>
+          <Text style={styles.picker}>Minuter</Text>
+        </View>
+        <View style={styles.boxContainerMid2}>
+          <TimePicker
+            selectedHours={selectedHours}
+            selectedMinutes={selectedMinutes}
+            onChange={(hours, minutes) =>
+              this.setState({
+                selectedHours: hours,
+                selectedMinutes: minutes
+              })
             }
           />
         </View>
+        <Text style={styles.infoText}>
+            Här kan du ändra när varningsljudet ska börja
+        </Text>
+        <View style={styles.boxContainerMid3}>
+            <Text style={styles.picker}>timmar</Text>
+            <Text style={styles.picker}>Minuter</Text>
+        </View>
+        <View style={styles.boxContainerMid4}>
+            <TimePicker
+              warningHours={warningHours}
+              warningMinutes={warningMinutes}
+              onChange={(hours, minutes) =>
+                this.setState({
+                  warningHours: hours,
+                  warningMinutes: minutes
+                })
+              }
+            />
+        </View>
+        <View style={styles.boxContainerBottom}>
+          <View style={styles.bottom}>
+            <AppSingleButton
+              title="Nästa"
+              onPress={() => {
+                const duration = this.toMillliseconds(
+                  this.state.selectedHours,
+                  this.state.selectedMinutes
+                );
+                this.saveSettings(duration);
+                this.props.navigation.navigate("HomeScreen", { duration});
+              }}
+            />
+          </View>
+        </View>
       </View>
     );
+  }
+  
+  toMillliseconds = (h, m) =>
+    moment.duration(h, "h").asMilliseconds() +
+    moment.duration(m, "m").asMilliseconds();
+
+  async saveSettings(time) {
+    try {
+      await AsyncStorage.setItem("time", JSON.stringify(time));
+    } catch (error) {
+      // Error saving data
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: "#fff",
+    justifyContent: "flex-start"
+  },
+  boxContainerTop: {
+    flex: 2
+  },
+  boxContainerMid1: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "flex-end"
+  },
+  boxContainerMid2: {
+    flex: 1
+  },
+  boxContainerMid3: {
+    flex: 1, 
+    flexDirection: "row",
+    alignItems: "flex-end"
+  },
+  boxContainerMid4: {
+    flex: 1
+  },
+  boxContainerBottom: {
+    flex: 1
   },
   heading: {
     fontSize: 35,
     flex: 1,
-    justifyContent: "flex-start",
     left: 10,
     marginTop: 50
   },
-  bottom: {
+  infoText: {
     flex: 1,
-    justifyContent: "flex-end"
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingRight: 20
+  },
+  picker: {
+    flex: 2,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    bottom: 0
+  },
+  bottom: {
+    position: "absolute",
+    width: "100%",
+    bottom: 0,
+    flex: 1,
   }
 });
--- pvt71/screens/WizardVerifyContactScreen.js
+++ pvt71/screens/WizardVerifyContactScreen.js
@@ -1,15 +1,35 @@
 import React from "react";
 import AppSingleButton from "../components/AppSingleButton";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput} from "react-native";
 
 export default class WizardVerifyContactScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {name: 'placeholder', number: '##########'};
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.heading}>Wizard Verify Contact Screen.</Text>
+        <Text style={styles.heading}>Sätt nödkontakt</Text>
+        <Text style={styles.infoText}>
+          {" "}
+          Nödkontakt namn: 
+        </Text>
+        <TextInput style={styles.textInput}
+        onChangeText={(name) => this.setState({name})}
+        value={this.state.name}
+        />
+        <Text style={styles.infoText}>
+          {" "}
+          Nödkontakt telefonnummer: 
+        </Text>
+        <TextInput style={styles.textInput}
+        onChangeText={(number) => this.setState({number})}
+        value={this.state.number}
+        />
         <View style={styles.bottom}>
           <AppSingleButton
-            title="Next"
+            title="Nästa"
             onPress={() =>
               this.props.navigation.navigate("WizardSettingsScreen")
             }
@@ -31,6 +51,20 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     left: 10,
     marginTop: 50
+  },
+  infoText: {
+    fontSize: 20,
+    marginBottom: 50,
+    paddingLeft: 10
+  },
+  textInput: {
+    height: 40,
+    paddingLeft: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 20,
+    borderColor: 'gray',
+    borderWidth: 1
   },
   bottom: {
     flex: 1,
