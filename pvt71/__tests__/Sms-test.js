be7ccdefb2976e0741bd25ee1b84df885857c6ff
LitillX <lovisa.hanserkers@gmail.com>
2019-05-29 22:16:36 +0200

    Now with 100% test branch coverage in utils.

--- pvt71/__tests__/Sms-test.js
+++ pvt71/__tests__/Sms-test.js
@@ -4,7 +4,7 @@ import { projectionParams } from '../utils/projection-params'
 import { smsSender } from '../utils/SmsSender';
 import assert from 'assert';
 
-describe('App snapshot', () => {
+describe('Sending sms and calculation position', () => {
 
     it('it calculates position', async () => {
         let position = await smsSender.contactEmergencyContact();
@@ -12,12 +12,22 @@ describe('App snapshot', () => {
     });
 
 
-    it('it calculates position', async () => {
+    it('it converts correctly', async () => {
         var x = 59.392542;
         var y = 17.906583;
         var params = projectionParams('sweref991800');
         var geo = geodeticToGrid(x, y, params);
         assert.equal(geo.x, 6586401);
         assert.equal(geo.y, 144692);
+    });
+
+    it('it handles defaultParams', async () => {
+        var x = 59.392542;
+        var y = 17.906583;
+        var params = projectionParams('defaultParam');
+        var geo = geodeticToGrid(x, y, params);
+        var geo = geodeticToGrid(x, y, params);
+        assert.equal(geo.x, null);
+        assert.equal(geo.y, null);
     });
 });
