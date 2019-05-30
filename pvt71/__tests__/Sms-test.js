314c8be50ad6466ea2728a1781e6b065e59c40ce
LitillX <lovisa.hanserkers@gmail.com>
2019-05-30 17:41:57 +0200

    feat/added mockups for fetch and AsyncStorage

--- pvt71/__tests__/Sms-test.js
+++ pvt71/__tests__/Sms-test.js
@@ -3,14 +3,39 @@ import { geodeticToGrid } from '../utils/geodetic-grid'
 import { projectionParams } from '../utils/projection-params'
 import { smsSender } from '../utils/SmsSender';
 import assert from 'assert';
+import MockStorage from '../__mocks__/mockStorage';
+
+const storageCache = {};
+const AsyncStorage = new MockStorage(storageCache);
+jest.setMock('AsyncStorage', AsyncStorage);
 
 describe('Sending sms and calculation position', () => {
+
+  beforeEach( async () => {
+    let contactPerson = {
+      firstName: "foo",
+      lastName: "bar",
+      number: 79584,
+    }
+    await AsyncStorage.setItem("contact", JSON.stringify(contactPerson));
+
+    global.fetch = jest.fn().mockImplementation(() => {
+      var p = new Promise((resolve, reject) => {
+        resolve({
+          ok: true,
+          json: function() {
+            return {ok: true}
+          }
+        });
+      });
+      return p;
+  });
+  });
 
     it('it calculates position', async () => {
         let position = await smsSender.contactEmergencyContact();
         assert.equal(true, position.includes("This is a call for help. "));
     });
-
 
     it('it converts correctly', async () => {
         var x = 59.392542;
@@ -20,6 +45,7 @@ describe('Sending sms and calculation position', () => {
         assert.equal(geo.x, 6586401);
         assert.equal(geo.y, 144692);
     });
+
 
     it('it handles defaultParams', async () => {
         var x = 59.392542;
